20a4c80c9c4849d93f36350025fb8ddf5ebbbcb6
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2024-05-17 23:09:20 -0300

    ajustes

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,38 @@
+package med.voll.api.controller;
+
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Transactional
+    @PostMapping
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        medicoRepository.save(new Medico(dados));
+
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+
+
+}
