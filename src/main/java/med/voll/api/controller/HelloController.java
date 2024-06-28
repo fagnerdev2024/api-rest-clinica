758333bd8499e0ae166269552c30102295cd35cf
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2024-06-27 21:07:21 -0300

    nova b

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String olaMundo(){
-        return "Hello World Spring Teste";
+        return "Hello World Spring NOVA BRANCH";
     }
 }
